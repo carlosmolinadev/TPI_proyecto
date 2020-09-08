ded8fc4b4db94ea040ba28c4e573f32aa81adb92
cmolina2089 <50473525+cmolina2089@users.noreply.github.com>
2020-09-07 22:44:13 -0600

    Initial commit

---
+++ includes/navBar.php
@@ -0,0 +1,40 @@
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+  <img src="../assets/images/logo.png" alt="logo" width="50px"><a class="navbar-brand"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
+    <ul class="navbar-nav">
+      <li class="nav-item  pr-5 active">
+        <a class="nav-link" href="#">Iniciar Sesion </a>
+      </li>
+      <li class="nav-item pr-5">
+        <a class="nav-link" href="#">Paquetes Turisticos</a>
+      </li>
+      <li class="nav-item pr-5 dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Rutas Turisticas
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="#">Playa</a>
+          <a class="dropdown-item" href="#">Montaña</a>
+          <a class="dropdown-item" href="#">Sitios Arqueologicos</a>
+        </div>
+      </li>
+      <li class="nav-item pr-5">
+        <a class="nav-link" href="#">Contactanos</a>
+      </li>
+      <li class="nav-item pr-5">
+        <a class="nav-link" href="#">
+          <span class="material-icons" style="font-size: 30px; color: rgba(255, 255, 255, 1);"> shopping_cart </span>  
+        </a>
+      </li>
+      <li class="nav-item pr-5">
+        <a class="nav-link" href="#">
+          <span class="material-icons" style="font-size: 30px; color: rgba(255, 255, 255, 1);"> account_circle </span>  
+        </a>
+      </li>
+    </ul>
+  </div>
+</nav>
---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+    define('TITLE', 'El Salvador Tours');
+    include('includes/header.php');
+    include('includes/navBar.php');
+?>
+
+    <a href="pages/logging.php">Sign In</a>
+    <i class="fa fa-shopping-cart" aria-hidden="true"></i>
+
+
+    <?php
+    include('includes/footer.php');
+    ?>  
+    
+
+
+
+
