5488cddeeafe31fd075887744d253a460dd2adb5
cmolina2089 <50473525+cmolina2089@users.noreply.github.com>
2020-09-09 15:55:26 -0600

    ''

---
+++ includes/dbh.php
@@ -0,0 +1,8 @@
+<?php
+
+$dbServerName = 'localhost';
+$dbUserName = 'root';
+$dbPassword = '1234';
+$dbName = 'tour_project';
+
+$conn = mysqli_connect($dbServerName, $dbUserName, $dbPassword, $dbName);
